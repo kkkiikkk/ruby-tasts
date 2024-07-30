5f2e3d6b6260bc303499ba72dc7a86716afe335f
kkkiikkk <ruddnovskiy@gmail.com>
2024-07-30 09:48:20 +0000

    feat: finish 3 task

---
+++ first-task/3.rb
@@ -0,0 +1,13 @@
+puts "Enter properties of matrix";
+puts "Column size: "
+columns = gets.chomp.to_i
+
+puts "Raws size: "
+raws = gets.chomp.to_i
+
+(0...raws).each do |i|
+  arr = [];
+  arr.fill(0, 0...columns);
+  arr[i] = 1 if i < columns
+  p arr.to_a
+end
