19110989c37c38742d16c177eb01bea2884e3d2d
kkkiikkk <ruddnovskiy@gmail.com>
2024-07-31 11:59:25 +0000

    finish third task

---
+++ thrid-task/soccer_league.rb
@@ -0,0 +1,54 @@
+def parse_string_games_to_array(game_string)
+  game_arr = game_string.split(",")
+  game_arr.each_with_index do |item, index| 
+    last_index = item.length - 1
+    name = item[0...last_index - 1]
+    game_arr[index] = [name.strip, item[last_index]]
+  end
+  game_arr
+end
+
+def generate_stats(parsed_games) 
+  stats = {}
+  parsed_games.each do |game|
+    stats[game[0][0]] = 0 if (!stats.key?(game[0][0]))
+    stats[game[1][0]] = 0 if (!stats.key?(game[1][0]))
+
+    if (game[0][1].to_i == game[1][1].to_i)
+      stats[game[0][0]] += 1
+      stats[game[1][0]] += 1
+      next
+    end
+
+    if (game[0][1].to_i < game[1][1].to_i)
+      stats[game[1][0]] += 3
+    else 
+      stats[game[0][0]] += 3
+    end
+  end
+
+  stats
+end
+
+def display_stats(stats) 
+  stats.each_with_index do |value, index|
+    puts "#{index + 1}. #{value[0]}, #{value[1]} #{value[1] == 1 ? "pt" : "pts"}"
+  end
+end
+
+def main() 
+  games = []
+  puts "Welcome to soccer league app"
+  puts "Enter ur games, format of the input game - Team points, Team points"
+  puts "Enter end when you will finish"
+  loop do 
+    game = gets.chomp.to_s
+    break if (game == "end")
+    games.push(parse_string_games_to_array(game))
+  end
+
+  generated_stats = generate_stats(games)
+  sorted_generated_stats = generated_stats.sort_by { |k, value| [-value, k] }
+
+  display_stats(sorted_generated_stats)
+end
---
+++ thrid-task/spec/soccer_league_spec.rb
@@ -0,0 +1,65 @@
+require_relative '../soccer_league.rb'
+
+# Mock data
+game_string_mock = "Lions 3, Snakes 3"
+game_arr_draw = [["Lions", "3"], ["Snakes", "3"]]
+game_arr_win = [["Lions", "2"], ["Snakes", "3"]]
+game_arr_lose = [["Lions", "3"], ["Snakes", "2"]]
+
+describe "Soccer League" do
+  describe "parse string game to array" do
+    context "given '#{game_string_mock}'" do
+      it "returns array of two elements each consisting of two elements, first is a team name, second is a score" do
+        game = parse_string_games_to_array(game_string_mock)
+        expect(game.size).to eq(2)
+        expect(game[0].size).to eq(2)
+        expect(game[1].size).to eq(2)
+        expect(game).to eq(game_arr_draw)
+      end
+    end
+  end
+
+  describe "generate stats" do
+    context "generate stats by the provided game" do
+      context "given '#{game_arr_draw}'" do
+        it "returns hash that has only two keys" do
+          stats = generate_stats([game_arr_draw])
+          expect(stats["Lions"]).to eq(1)
+          expect(stats["Snakes"]).to eq(1)
+        end
+      end
+
+      context "given a winning game '#{game_arr_win}'" do
+        it "returns hash with correct points allocation" do
+          stats = generate_stats([game_arr_win])
+          expect(stats["Lions"]).to eq(0)
+          expect(stats["Snakes"]).to eq(3)
+        end
+      end
+
+      context "given a losing game '#{game_arr_lose}'" do
+        it "returns hash with correct points allocation" do
+          stats = generate_stats([game_arr_lose])
+          expect(stats["Lions"]).to eq(3)
+          expect(stats["Snakes"]).to eq(0)
+        end
+      end
+
+      context "given multiple games" do
+        it "aggregates the stats correctly" do
+          games = [game_arr_draw, game_arr_win, game_arr_lose]
+          stats = generate_stats(games)
+          expect(stats["Lions"]).to eq(4)
+          expect(stats["Snakes"]).to eq(4)
+        end
+      end
+    end
+  end
+
+  describe "display stats" do
+    it "displays the stats in the correct format" do
+      stats = {"Lions" => 4, "Snakes" => 4}
+      expect { display_stats(stats) }.to output("1. Lions, 4 pts\n2. Snakes, 4 pts\n").to_stdout
+    end
+  end
+end
